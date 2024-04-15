5493ed473b173ffcb75ec4a212bdf75ba382133c
Cristian Fernando Narváez Sánchez <cristianfns11@gmail.com>
2024-04-15 17:27:02 -0500

    mua

--- tecnico/abril/Codigos Funciones/16.imprime5/js/imprime5.js
+++ tecnico/abril/Codigos Funciones/16.imprime5/js/imprime5.js
@@ -4,25 +4,27 @@
  * Fecha: jueves 04 de abril de 2024
  * 
  */
-function calNum(pcontador,pnumero){
-    let contador = pcontador;
+function calNum(pnumero){
     let numero = pnumero;
+    let contador = 0;
+    let resultadoC = ""
     while(contador<numero){
         contador++;
-        console.log(contador);
+        resultadoC += contador+"\n";
     }
-    
+    return resultadoC;
 }
 
 /**
  * constante 
  */
-const calNum2=function(pcontador,pnumero){
-    let contador = pcontador;
+const calNum2=function(pnumero){
     let numero = pnumero;
+    let contador = 0;
+    let resultadoC = ""
     while(contador<numero){
         contador++;
-        console.log(contador);
+        resultadoC += contador+"\n";
     }
-    
+    return resultadoC;
 }
