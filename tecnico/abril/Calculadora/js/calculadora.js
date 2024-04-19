09f594dae87c1dd3c3a92f40cb901e5eaa560ac8
SERGIO-PC <vargasleguizamo95@gmail.com>
2024-04-19 11:25:24 -0500

    calcu

---
+++ tecnico/abril/Calculadora/js/calculadora.js
@@ -0,0 +1,26 @@
+
+
+let displayValue = '';
+
+function appendValue(value) {
+  displayValue += value;
+  document.getElementById('display').value = displayValue;
+}
+
+function deleteDigit() {
+    displayValue = displayValue.slice(0, -1);
+    document.getElementById('display').value = displayValue;
+  }
+function clearDisplay() {
+  displayValue = '';
+  document.getElementById('display').value = displayValue;
+}
+
+function calculate() {
+  try {
+    displayValue = eval(displayValue);
+    document.getElementById('display').value = displayValue;
+  } catch (error) {
+    document.getElementById('display').value = 'Error';
+  }
+}
