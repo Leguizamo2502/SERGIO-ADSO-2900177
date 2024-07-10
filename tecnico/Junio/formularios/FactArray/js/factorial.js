c7f8065edcb18fd2150aa3a2242def6ee74b5172
SERGIO-PC <vargasleguizamo95@gmail.com>
2024-07-10 14:09:03 -0500

    fact

---
+++ tecnico/Junio/formularios/FactArray/js/factorial.js
@@ -0,0 +1,23 @@
+/**
+ * Sergio Leguizamo
+ */
+
+let arreglo = [];
+let iteracion;
+let factorial;
+let numero2;
+let resultaLista = "";
+
+numero2 = 5;
+factorial = 1;
+
+for(iteracion = 1; iteracion <= numero2; iteracion++){
+    factorial = factorial * iteracion;
+    arreglo.push({ numero: iteracion, factorial: factorial });
+}
+
+for (iteracion = 0; iteracion < arreglo.length; iteracion++) {
+    resultaLista += `<tr><td>${arreglo[iteracion].numero}</td><td>${arreglo[iteracion].factorial}</td></tr>`; 
+}
+
+document.getElementById('lista').innerHTML = resultaLista;
